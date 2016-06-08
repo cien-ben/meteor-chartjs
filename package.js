b1fbb611945e242ae7b19fdcf5407335ca882fe5
cien-ben <ben.strum@gmail.com>
2016-06-07 20:58:43 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,6 @@
 Package.describe({
-  summary: 'Simple HTML5 Charts using the <canvas> tag'
+  summary: 'Simple HTML5 Charts using the <canvas> tag',
+  git: 'https://github.com/cien-ben/meteor-chartjs.git'
 });
 
 Package.on_use(function (api) {
